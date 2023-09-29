e2856ee144ed0b1f85a76545a275a4dc949aa223
Shanit Paul <shanit.kumarpaul.20@aot.edu.in>
2023-09-30 00:40:33 +0530

    11

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 const express= require('express')
 const mongoose= require('mongoose')
+const cors = require('cors');
 const noteRoutes=require('./routes/note')
 const userRoutes=require('./routes/user')
 
 //express app
 const app= express()
 
+app.use(cors());
 //middleware
 app.use(express.json())
 
