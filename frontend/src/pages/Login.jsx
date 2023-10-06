ee5828fadacc00efca85e809902fcdcb185525b4
Shanit Paul <shanit.kumarpaul.20@aot.edu.in>
2023-10-06 20:19:38 +0530

    password info

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,11 +5,11 @@ import { useLogin } from '../hooks/useLogin'
 export const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const {login,error,isLoading}=useLogin()
+    const { login, error, isLoading } = useLogin()
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        await login(email,password)
+        await login(email, password)
     }
 
     return (
@@ -18,18 +18,20 @@ export const Login = () => {
             <form className="space-y-4 md:space-y-6 flex flex-col justify-center items-center w-60 md:w-80" onSubmit={handleSubmit}>
                 <div className="w-full">
                     <label className="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Your email</label>
-                    <input onChange={(e)=>setEmail(e.target.value)} type="email" value={email} id="email" className="bg-gray-50  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-sky-800  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true} />
+                    <input onChange={(e) => setEmail(e.target.value)} type="email" value={email} id="email" className="bg-gray-50  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-sky-800  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true} />
                 </div>
                 <div className="w-full">
                     <label className="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Password</label>
-                    <input onChange={(e)=>setPassword(e.target.value)} type="password" value={password} id="password" placeholder="• • • • • • • • • • •" className="bg-gray-50  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-sky-800  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} />
+                    <input onChange={(e) => setPassword(e.target.value)} type="password" value={password} id="password" placeholder="• • • • • • • • • • •" className="bg-gray-50  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-sky-800  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} />
                 </div>
 
                 <button disabled={isLoading} className="w-1/2 text-white dark:text-black bg-sky-800 hover:bg-sky-700  font-bold rounded-lg  px-5 py-2.5 text-center dark:bg-sky-300 dark:hover:bg-sky-400 ">Login</button>
                 <div className="text-sm font-light text-gray-600 dark:text-gray-400">
                     Don't have an account? <Link to="/signup" className="font-bold text-sky-800 hover:underline dark:text-sky-500">Signup here</Link>
                 </div>
-                {error && <div className="text-red-500 text-xl">{error}</div>}
+                {error && <div className="text-red-500 text-xl">
+                    {error}
+                </div>}
             </form>
         </div>
     )
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -12,13 +12,10 @@ export const Signup = () => {
         e.preventDefault()
         await signup(email,password)
     }
-    // const passwordCheck= () => {
-
-    // }
 
     return (
         <div className="w-full h-screen flex flex-col justify-center items-center">
-            <div className="text-2xl md:text-3xl font-semibold dark:text-white mb-10">Signup here</div>
+            <div className="text-2xl md:text-3xl font-semibold dark:text-white mt-16 mb-8">Signup here</div>
             <form className="space-y-4 md:space-y-6 flex flex-col justify-center items-center w-60 md:w-80" onSubmit={handleSubmit}>
                 <div className="w-full">
                     <label className="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Your email</label>
@@ -28,23 +25,30 @@ export const Signup = () => {
                     <label className="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Password</label>
                     <input onChange={(e)=>setPassword(e.target.value)} type="password" value={password} id="password" placeholder="• • • • • • • • • • •" className="bg-gray-50  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-sky-800  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} />
                 </div>
-                {/* <div className="w-full">
-                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                    <input onChange={(e)=>setConfirmPassword(e.target.value)} type="confirmPassword" value={confirmPassword} id="confirmPassword" placeholder="••••••••" className="bg-gray-50  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-sky-800  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} />
-                </div> */}
-                {/* <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4  rounded bg-gray-50 focus:ring-3  dark:bg-sky-800  dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                    </div>
-                    <div className="ml-3 text-sm">
-                        <label className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="/">Terms and Conditions</a></label>
-                    </div>
-                </div> */}
                 <button type="submit" disabled={isLoading} className="w-fit text-white dark:text-black bg-sky-900 hover:bg-sky-800  font-bold rounded-lg px-5 py-2.5 text-center dark:bg-sky-300 dark:hover:bg-sky-400 ">Create an account</button>
                 <div className="text-sm font-light text-gray-600 dark:text-gray-400">
                     Already have an account? <Link to="/login" className="font-bold text-sky-700 hover:underline dark:text-sky-500">Login here</Link>
                 </div>
-                {error && <div className="text-red-500 text-xl">{error}</div>}
+                {error && <div className="text-red-500 text-xl">
+                    {error}
+                    <div className="w-72 h-fit border-2 border-red-400 bg-red-200 p-3 text-xs rounded-lg mt-4">
+                        <div className="">
+                            1. <strong>Length:</strong> A strong password usually has a minimum length, often 8 characters or more.
+                        </div>
+                        <div className="">
+                            2. <strong>Uppercase Letters:</strong> Including at least one uppercase letter.
+                        </div>
+                        <div className="">
+                            3. <strong>Lowercase Letters:</strong> Including at least one lowercase letter.
+                        </div>
+                        <div className="">
+                            4. <strong>Numbers:</strong> Including at least one numerical digit.
+                        </div>
+                        <div className="">
+                            5. <strong>Special Characters:</strong> Including at least one special character (e.g., !, @, #, $, etc.).
+                        </div>
+                    </div>    
+                </div>}
             </form>
         </div>
     )
